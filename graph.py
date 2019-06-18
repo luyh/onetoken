4307692e3db810c5b1338fbef303ce330a067ae7
luyh <yanhonglu5@icloud.com>
2019-06-18 19:58:58 +0800

    生成节点对应三方交易对汇率

--- graph.py
+++ graph.py
@@ -10,6 +10,10 @@ class Node:
 
         self.contract = pd.DataFrame()      #DataFrame入节点，边：dict:名称:节点，边 ,入度
 
+        self.mid = pd.DataFrame()
+
+        self.path = pd.DataFrame()
+
 
 
 # 图的边结构
@@ -19,6 +23,8 @@ class Edge:
         self.to = to                # 边的to节点
 
         self.rate = rate
+
+        self.df = pd.DataFrame()
 
 def get_price(contract):
     print(contract)
@@ -41,38 +47,10 @@ def get_price(contract):
 class Graph:
     def __init__(self):
         self.nodes = {}     # 图的所有节点集合  字典形式：{节点编号：节点}
-        self.edges = {}    # 图的边集合
+        self.edges = pd.DataFrame()    # 图的边集合
 
+        self.pah = {}
 
-# 生成图结构
-# matrix = [
-#   [1,2,3],        ==>   里面分别代表权重, from节点, to节点
-#   [...]
-# ]
-
-'''
-def createGraph(matrix):
-    graph = Graph()
-    for edge in matrix:
-        weight = edge[0]
-        fro = edge[1]
-        to = edge[2]
-        if fro not in graph.nodes:
-            graph.nodes[fro] = Node(fro)
-        if to not in graph.nodes:
-            graph.nodes[to] = Node(to)
-        fromNode = graph.nodes[fro]
-        toNode = graph.nodes[to]
-        newEdge = Edge(weight, fromNode, toNode)
-        fromNode.nexts.append(toNode)
-        fromNode.out += 1
-        toNode.come += 1
-        fromNode.edges.append(newEdge)
-        graph.edges.append(newEdge)
-    return graph
-'''
-def createGraph():
-    pass
 
 def demo():
     DEBUG = True
@@ -108,35 +86,46 @@ def demo():
 
         newEdge0 = Edge( Node0, Node1)
         newEdge1 = Edge(Node1, Node0)
+        dict0 = {
+            'fro': pair[0],
+            'to':  pair[1],
+            'ask_price':ask_price,
+            'bid_price':bid_price
+        }
+        newEdge0.df = pd.DataFrame(dict0)
+        print(newEdge0.df)
 
-        newEdge0.rate['ask_price'] = ask_price
-        newEdge0.rate['bid_price'] = bid_price
+        dict1 = {
+            'fro': pair[1],
+            'to':  pair[0],
+            'ask_price':1 / bid_price,
+            'bid_price':1 / ask_price
+        }
+        newEdge1.df = pd.DataFrame(dict1)
+        print(newEdge1.df)
+        graph.edges = pd.concat([graph.edges, newEdge0.df, newEdge1.df])
 
-        newEdge1.rate['ask_price'] = 1 / bid_price
-        newEdge1.rate['bid_price'] = 1 / ask_price
+        contract_df0 = pd.DataFrame(newEdge0.df[['to','ask_price','bid_price']])
+        print(contract_df0)
+        Node0.contract = pd.concat([Node0.contract,contract_df0])
+        #print(Node0.contract)
 
-        contract0 = {'contract':pair[1],
-               'ask':newEdge0.rate['ask_price'],
-               'bid':newEdge0.rate['bid_price']
-               }
-
-        contract_df = pd.DataFrame(contract0)
-        print(contract_df)
-        Node0.contract = pd.concat([Node0.contract,contract_df])
-        print(Node0.contract)
-
-        contract1 = {'contract':pair[0],
-               'ask':newEdge1.rate['ask_price'],
-               'bid':newEdge1.rate['bid_price']
-               }
-
-        contract_df1 = pd.DataFrame(contract1)
+        contract_df1 = pd.DataFrame(newEdge1.df[['to','ask_price','bid_price']])
         print(contract_df1)
         Node1.contract = pd.concat([Node1.contract,contract_df1])
-        print(Node1.contract)
+        #print(Node1.contract)
 
-        graph.edges[pair[0], pair[1]] = ( newEdge0 )
-        graph.edges[pair[1], pair[0]] = (newEdge1)
+    for node in graph.nodes:
+        print(node)
+        Node0 = graph.nodes[node]
+        df = Node0.contract
+        print(df)
+
+        fro = graph.edges[graph.edges['fro'].isin(df['to'])]
+        Node0.mid = fro[fro['to'].isin(df['to'])]
+        print(Node0.mid)
+
+        print('debug')
 
     print( 'End' )
 
